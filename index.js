818f89e027b2c256342bf3dff33ba64ab14d9425
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-02-14 22:22:53 -0500

    Added code to generate a readme file with test text.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const fs = require('fs');
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("./" + fileName, data, err => {
+        if (err) throw new Error(err);
+        console.log('README complete! Check out README.md to see the output!');
+        });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile("README.md", "Test file.");
+}
 
 // Function call to initialize app
 init();
