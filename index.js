05e41f0989dfa574c97464bb80d5366089c770b0
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-02-14 23:42:05 -0500

    Added some test questions.

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const fs = require('fs');
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of the project?'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is the user story?'
+  },
+  {
+    type: 'input',
+    name: 'about',
+    message: 'Description of the projects: '
+  },
+  {
+    type: 'editor',
+    name: 'completion',
+    message: 'Description of the steps for completion: '
+  },{
+    type: 'list',
+    name: 'license',
+    message: 'What tipe of license does your project operate under? ',
+    choices: ['None', 'Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC', 'GNU AGPLv3', 'GNU LGPLv3', 'Boost Software License 1.0', 'Mozilla Public License 2.0', 'The Unlicense'],
+    default: 0
+  }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -14,7 +40,11 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile("README.md", "Test file.");
+    promptInfo().then(answers => console.log(answers));
+}
+
+function promptInfo(){
+    return inquirer.prompt(questions) 
 }
 
 // Function call to initialize app
